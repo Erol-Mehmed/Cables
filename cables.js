29a53e0d7b34f09f4c65a0a8ed5d5afedb8230b3
Erol-web <81830899+Erol-web@users.noreply.github.com>
2022-04-05 18:45:11 +0300

    refactor

--- cables.js
+++ cables.js
@@ -1,132 +1,97 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function htmInput() {
+  // This function takes the user input.
+
   let inputArray = document.getElementById("input").value;
 
   inputChecking(inputArray);
 }
 
 function inputChecking(inputArray) {
+  // This function checks if the user input is valid or not.
 
-  let checkArr = inputArray.split('');
+  let checkArr = inputArray.split("");
 
-  let checkForSpacesOnly = checkArr.every(el => el === ' ');
+  let countOfNumbers = inputArray.split(" ").length;
+
+  let checkForSpacesOnly = checkArr.every((el) => el === " ");
 
   let checkForStrings = false;
 
-  checkArr.forEach(el => isNaN(el) === true ? checkForStrings = true : false);
+  checkArr.forEach((el) =>
+    isNaN(el) === true ? (checkForStrings = true) : false
+  );
 
   if (checkForSpacesOnly || checkForStrings) {
-
     return alert(`You must enter integer numbers separated by space!`);
-
+  } else if (countOfNumbers === 1) {
+    return alert(
+      `There must be more than ONE cables! A space must be added between cables!`
+    );
   } else {
-
-    arrayClearing(inputArray);
-
+    displayInput(inputArray);
   }
+}
 
+function displayInput(inputArray) {
+  // This function displays the input numbers.
+
+  let inputElement = document.getElementById("inputNumbers");
+
+  inputElement.innerHTML = `You entered: ${inputArray}`;
+
+  inputElement.style.display = "inline-block";
+
+  arrayClearing(inputArray);
 }
 
 function arrayClearing(inputArray) {
+  // This function clears the input array from all spaces and zeros so only the numbers bigger than zero are left.
 
   inputArray = inputArray.split(" ").map(Number);
 
-  inputArray = inputArray.filter(el => el !== 0);
+  inputArray = inputArray.filter((el) => el !== 0);
 
-  arrayChecking(inputArray);
-
-}
-
-function arrayChecking(inputArray) {
-
-  if (inputArray.length === 1) {
-
-    return alert(`There must be more than ONE cables! A space must be added between cables!`);
-
-  } else {
-
-    trimming(inputArray);
-
-  }
-
+  trimming(inputArray);
 }
 
 function trimming(inputArray) {
+  // This function makes the trimming of the cables.
 
   let counter = 0;
 
   while (inputArray.length > 0) {
+    inputArray.sort((a, b) => b - a);
 
-      inputArray.sort((a, b) => b - a);
+    inputArray = inputArray.map(
+      (num) => num - inputArray[inputArray.length - 1]
+    );
 
-      inputArray = inputArray.map(num => num - inputArray[inputArray.length - 1]);
+    inputArray.forEach((el) => counter++);
 
-      inputArray.forEach(el => counter++);
-
-      inputArray = inputArray.filter(el => el > 0);
-
+    inputArray = inputArray.filter((el) => el > 0);
   }
 
   displayOutput(counter);
-
 }
 
 function displayOutput(counter) {
+  // This function displays the output.
 
-  let outputElement = document.getElementById('output');
+  let outputElement = document.getElementById("output");
 
   outputElement.innerHTML = `We have performed ${counter} trims`;
 
-  outputElement.style.display = 'inline-block';
-
+  outputElement.style.display = "inline-block";
 }
 
-let submitElement = document.getElementById('submit');
+// The code below takes the submit button element and attach event listener to it.
 
-submitElement.addEventListener('click', e => {
+let submitElement = document.getElementById("submit");
 
+submitElement.addEventListener("click", (e) => {
   e.preventDefault();
 
   htmInput();
 
   document.getElementById("cables").reset();
-
 });
-
-
